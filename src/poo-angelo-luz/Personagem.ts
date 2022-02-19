52f2a9f28fa83d85b2e87a217d8a90ac48bd9e60
Gildo Neto <gildoneto@hotmail.com>
2022-02-19 16:38:13 -0300

    organiza arquivos

--- src/poo-angelo-luz/Personagem.ts
+++ src/poo-angelo-luz/Personagem.ts
@@ -1,33 +1,38 @@
-// nome, energia, vida, ataque, defesa
+import prompt from "prompt-sync";
 
-//classe
 class Personagem {
-  nome: string = '';
-  energia: number = 0;
-  vida: number = 0;
-  ataque: number = 0;
-  defesa: number = 0;
+  constructor(
+    public nome: string, 
+    public energia: number, 
+    public vida: number, 
+    public ataque: number, 
+    public defesa: number
+  ) {}
 }
 
-//atributos
+let sansa: Personagem = new Personagem('Sansa Stark', 100, 40, 20, 20);
 
+let teclado = prompt();
+let option: number = 0;
 
-//métodos (funções)
-let nome = 'Jon';
+while (option != 9) {
+    console.log("+========= Personagem =============+");
+    console.log("|1. Treinar ataque                 |");
+    console.log("|2. Treinar defesa                 |");
+    console.log("|3. Imprimir atributos             |");
+    console.log("|9. Sair                           |");
+    console.log("+==================================+");
 
-let sansa: Personagem;
-sansa = new Personagem();
-sansa.nome = 'Sansa Stark';
-sansa.ataque = 10;
-sansa.defesa = 20;
-sansa.energia = 20;
-sansa.vida = 100;
-console.log('SANSA: ', sansa);
+    option = +teclado("Escolha uma ação: ");
 
-let jon: Personagem = new Personagem();
-jon.nome = 'Jon Snow';
-jon.ataque = 80;
-jon.defesa = 70;
-jon.energia = 90;
-jon.vida = 40;
-console.log('JON: ', jon);
+    switch (option) {
+        case 1:
+            sansa.ataque += 2;
+            break;
+        case 3:
+            console.log("sansa :>> ", sansa);
+            break;
+        default:
+            break;
+    }
+}
---
+++ src/poo-angelo-luz/readme.md
@@ -0,0 +1,17 @@
+# Orientação a Objetos com TypeScript
+#### por Angelo Luz -> [Playlist](https://www.youtube.com/watch?v=G5TOitIft8o&list=PLDqnSpzNKDvnh-0RCYbIL5WzGCCFV1Ghm)
+
+
+## Pacotes NPM
+
+ts-node -> roda código .ts sem criar o arquivo .js
+Para instalar: `npm install -g ts-node`
+Para rodar: `ts-node nomeDoArquivo.ts`
+
+prompt-sync ->
+Para instalar:
+`npm install prompt-sync`
+`npm install @types/prompt-sync`
+Para rodar precisa importar dentro do arquivo:
+`import prompt from 'prompt-sync'`
+`let teclado = prompt()`
